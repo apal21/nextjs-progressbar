9ff0eb8ea1dd2d0ef181ac2b64ab312522055627
Parbez <imranbarbhuiya.fsd@gmail.com>
2022-10-12 17:55:26 +0530

    feat: use react.memo and allow custom css tag (#85)
    * chore: migrate to yarn v3
    
    * perf: use react.memo
    
    * feat: allow custom css tag for minify
    
    * chore: update changelog
    
    * fix: change to transformCSS
    
    * chore: update readme
    
    * fix: proptype
    
    * chore: bump deps
    
    * 0.0.16

--- src/index.tsx
+++ src/index.tsx
@@ -9,15 +9,7 @@ import * as NProgress from 'nprogress';
 import * as PropTypes from 'prop-types';
 import * as React from 'react';
 
-export default function NextNProgress({
-  color = '#29D',
-  startPosition = 0.3,
-  stopDelayMs = 200,
-  height = 3,
-  showOnShallow = true,
-  options,
-  nonce,
-}: {
+export interface NextNProgressProps {
   /**
    * The color of the bar.
    * @default "#29D"
@@ -53,7 +45,25 @@ export default function NextNProgress({
    * @default undefined
    */
   nonce?: string;
-}) {
+
+  /**
+   * Use your custom CSS tag instead of the default one.
+   * This is useful if you want to use a different style or minify the CSS.
+   * @default (css) => <style nonce={nonce}>{css}</style>
+   */
+  transformCSS?: (css: string) => JSX.Element;
+}
+
+const NextNProgress = ({
+  color = '#29D',
+  startPosition = 0.3,
+  stopDelayMs = 200,
+  height = 3,
+  showOnShallow = true,
+  options,
+  nonce,
+  transformCSS = (css) => <style nonce={nonce}>{css}</style>,
+}: NextNProgressProps) => {
   let timer: NodeJS.Timeout | null = null;
 
   React.useEffect(() => {
@@ -117,77 +127,75 @@ export default function NextNProgress({
     }
   };
 
-  return (
-    <style nonce={nonce}>{`
-      #nprogress {
-        pointer-events: none;
+  return transformCSS(`
+    #nprogress {
+      pointer-events: none;
+    }
+    #nprogress .bar {
+      background: ${color};
+      position: fixed;
+      z-index: 9999;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: ${height}px;
+    }
+    #nprogress .peg {
+      display: block;
+      position: absolute;
+      right: 0px;
+      width: 100px;
+      height: 100%;
+      box-shadow: 0 0 10px ${color}, 0 0 5px ${color};
+      opacity: 1;
+      -webkit-transform: rotate(3deg) translate(0px, -4px);
+      -ms-transform: rotate(3deg) translate(0px, -4px);
+      transform: rotate(3deg) translate(0px, -4px);
+    }
+    #nprogress .spinner {
+      display: block;
+      position: fixed;
+      z-index: 1031;
+      top: 15px;
+      right: 15px;
+    }
+    #nprogress .spinner-icon {
+      width: 18px;
+      height: 18px;
+      box-sizing: border-box;
+      border: solid 2px transparent;
+      border-top-color: ${color};
+      border-left-color: ${color};
+      border-radius: 50%;
+      -webkit-animation: nprogresss-spinner 400ms linear infinite;
+      animation: nprogress-spinner 400ms linear infinite;
+    }
+    .nprogress-custom-parent {
+      overflow: hidden;
+      position: relative;
+    }
+    .nprogress-custom-parent #nprogress .spinner,
+    .nprogress-custom-parent #nprogress .bar {
+      position: absolute;
+    }
+    @-webkit-keyframes nprogress-spinner {
+      0% {
+        -webkit-transform: rotate(0deg);
       }
-      #nprogress .bar {
-        background: ${color};
-        position: fixed;
-        z-index: 9999;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: ${height}px;
+      100% {
+        -webkit-transform: rotate(360deg);
       }
-      #nprogress .peg {
-        display: block;
-        position: absolute;
-        right: 0px;
-        width: 100px;
-        height: 100%;
-        box-shadow: 0 0 10px ${color}, 0 0 5px ${color};
-        opacity: 1;
-        -webkit-transform: rotate(3deg) translate(0px, -4px);
-        -ms-transform: rotate(3deg) translate(0px, -4px);
-        transform: rotate(3deg) translate(0px, -4px);
+    }
+    @keyframes nprogress-spinner {
+      0% {
+        transform: rotate(0deg);
       }
-      #nprogress .spinner {
-        display: block;
-        position: fixed;
-        z-index: 1031;
-        top: 15px;
-        right: 15px;
+      100% {
+        transform: rotate(360deg);
       }
-      #nprogress .spinner-icon {
-        width: 18px;
-        height: 18px;
-        box-sizing: border-box;
-        border: solid 2px transparent;
-        border-top-color: ${color};
-        border-left-color: ${color};
-        border-radius: 50%;
-        -webkit-animation: nprogresss-spinner 400ms linear infinite;
-        animation: nprogress-spinner 400ms linear infinite;
-      }
-      .nprogress-custom-parent {
-        overflow: hidden;
-        position: relative;
-      }
-      .nprogress-custom-parent #nprogress .spinner,
-      .nprogress-custom-parent #nprogress .bar {
-        position: absolute;
-      }
-      @-webkit-keyframes nprogress-spinner {
-        0% {
-          -webkit-transform: rotate(0deg);
-        }
-        100% {
-          -webkit-transform: rotate(360deg);
-        }
-      }
-      @keyframes nprogress-spinner {
-        0% {
-          transform: rotate(0deg);
-        }
-        100% {
-          transform: rotate(360deg);
-        }
-      }
-    `}</style>
-  );
-}
+    }
+  `);
+};
 
 NextNProgress.propTypes = {
   color: PropTypes.string,
@@ -197,4 +205,7 @@ NextNProgress.propTypes = {
   showOnShallow: PropTypes.bool,
   options: PropTypes.object,
   nonce: PropTypes.string,
+  transformCSS: PropTypes.func,
 };
+
+export default React.memo(NextNProgress);
